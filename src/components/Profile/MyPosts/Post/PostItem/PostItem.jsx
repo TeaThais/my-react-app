dd34c7412655a883a0fa8b1ad93b287f946877d2
Tais <72388962+TeaThais@users.noreply.github.com>
2022-07-28 13:33:52 +0300

    Update PostItem.jsx

--- src/components/Profile/MyPosts/Post/PostItem/PostItem.jsx
+++ src/components/Profile/MyPosts/Post/PostItem/PostItem.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import classes from './PostItem.module.css';
 
 const PostItem = (props) => {
-    console.log(props)
+
+
+
     return <div className={classes.item}>
         <div>
-            <img className={classes.img} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEMpUS3NxMp4i7QkSKNn40Oax_w-mLBFsoPA&usqp=CAU" alt=""/>
-            {props.name} {props.message}
+            {props.pic} {props.name} {props.message}
         </div>
         <div>
             <button className={classes.button}>Like</button>
