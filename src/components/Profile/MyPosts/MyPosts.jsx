1242231289b4eae9d918cdba23f03bdfe75f9d51
Tais <72388962+TeaThais@users.noreply.github.com>
2022-07-28 13:33:32 +0300

    Update MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,7 @@ import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
-    return <div> my posts
+    return <div>
             <Post />
         </div>
 }
