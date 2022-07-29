672dd678c70b24159799de8b8f8e3df5af8607e8
Tais <72388962+TeaThais@users.noreply.github.com>
2022-07-29 10:50:55 +0300

    Update Navbar.jsx
    New way to use className

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return <nav className={classes.nav}>
-        <div className= {`${classes.active} ${classes.item}`}>
-            <a>Profile</a></div>
+        <div className= {classes.item}>
+            <NavLink to='/profile' className = {navData => navData.isActive ? classes.active : classes.item }>Profile</NavLink></div>
         <div className={classes.item}>
-            <a>Messages</a>
+            <NavLink to='/dialogs' className = {navData => navData.isActive ? classes.active : classes.item }>Messages</NavLink>
         </div>
         <div className={classes.item}>
-            <a>News</a>
+            <NavLink to='/news' className = {navData => navData.isActive ? classes.active : classes.item }>News</NavLink>
         </div>
         <div className={classes.item}>
-            <a>Music</a>
+            <NavLink to='/music' className = {navData => navData.isActive ? classes.active : classes.item }>Music</NavLink>
         </div>
         <div className={classes.item}>
-            <a>Settings</a>
+            <NavLink to='/settings' className = {navData => navData.isActive ? classes.active : classes.item }>Settings</NavLink>
         </div>
     </nav>
 }
