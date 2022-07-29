ba2fc19cfcd6209fc29dd10e151934ebd31faee9
Tais <72388962+TeaThais@users.noreply.github.com>
2022-07-29 10:49:46 +0300

    Update App.js
    New requirements using <Routes></Routes>

--- src/App.js
+++ src/App.js
@@ -5,22 +5,31 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
 
 const App = () => {  //App is a function that returns layout
-  return (
-  <div className='app-wrapper'>
-    <Header />
-    <Navbar />
-    <div className='content'>
-
-      <Profile />
-      {/*<Dialogs /> */}
-    </div>
-  </div>);
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='content'>
+                    <Routes>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                        <Route path="/music" element={<Music/>}/>
+                        <Route path="/news" element={<News/>}/>
+                        <Route path="/settings" element={<Settings/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
-
 
 
 export default App;
