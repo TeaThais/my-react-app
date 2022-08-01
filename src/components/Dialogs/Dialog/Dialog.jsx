f69df0154c1370de2d17159f58fe548d7544b020
Tais <72388962+TeaThais@users.noreply.github.com>
2022-08-01 21:18:32 +0300

    Update Dialog.jsx

--- src/components/Dialogs/Dialog/Dialog.jsx
+++ src/components/Dialogs/Dialog/Dialog.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import classes from './Dialog.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialog = (props) => {
+
+    let path = '/dialogs/' + props.id
+
     return (
         <div>
-            <div>
-                {props.pic} {props.name} {props.message}
+            <div className={classes.dialogItem}>
+                <NavLink to={path}>{props.name} {props.message}</NavLink>
             </div>
         </div>
     )
 }
 
 export default Dialog;
+
+
