2753623f12778d14b5afc7b8d735c8e39b735de7
Tais <72388962+TeaThais@users.noreply.github.com>
2022-08-22 15:18:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {Routes, Route, BrowserRouter} from "react-router-dom";
+import {Route, BrowserRouter} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
@@ -18,13 +18,11 @@ const App = () => {  //App is a function that returns layout
                 <Header/>
                 <Navbar/>
                 <div className='content'>
-                    <Routes>
-                        <Route exact path="/profile" element={<Profile/>}/>
-                        <Route exact path="/dialogs" element={<Dialogs/>}/>
-                        <Route exact path="/music" element={<Music/>}/>
-                        <Route exact path="/news" element={<News/>}/>
-                        <Route exact path="/settings" element={<Settings/>}/>
-                    </Routes>
+                        <Route path="/profile" component={Profile}/>
+                        <Route path="/dialogs" component={Dialogs}/>
+                        <Route path="/music" component={Music}/>
+                        <Route path="/news" component={News}/>
+                        <Route path="/settings" component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
