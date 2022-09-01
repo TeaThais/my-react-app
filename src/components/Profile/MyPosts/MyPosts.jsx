9fa1f10dfa44fa8579e3a0fd745d6786961555a5
Tais <72388962+TeaThais@users.noreply.github.com>
2022-09-01 20:54:23 +0300

    Update MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
+import postItem from "./Post/PostItem/PostItem";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
     return <div>
-            <Post />
+            <Post postItems={props.postItems}/>
         </div>
 }
 
