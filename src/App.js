499b2146f4a458a29c7a904a53f55dd38e173248
Tais <72388962+TeaThais@users.noreply.github.com>
2022-09-01 20:53:58 +0300

    Update App.js
    Render instead of component

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {Route, BrowserRouter} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
+import {Route, BrowserRouter} from "react-router-dom";
 
 
-const App = () => {  //App is a function that returns layout
+const App = (props) => {  //App is a function that returns layout
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
                 <div className='content'>
-                        <Route path="/profile" component={Profile}/>
-                        <Route path="/dialogs" component={Dialogs}/>
-                        <Route path="/music" component={Music}/>
-                        <Route path="/news" component={News}/>
-                        <Route path="/settings" component={Settings}/>
+                        <Route path="/profile" render={() => <Profile avatarPic={props.avatarPic} backgroundPic={props.backgroundPic} postItems={props.postItems}/>}/>
+                        <Route path="/dialogs" render={() => <Dialogs dialogs={props.dialogs} messagesData={props.messagesData}/>}/>
+                        <Route path="/music" render={() => <Music />}/>
+                        <Route path="/news" render={() => <News />}/>
+                        <Route path="/settings" render={() => <Settings />}/>
                 </div>
             </div>
         </BrowserRouter>
