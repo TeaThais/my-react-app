9967ab8e08fabb06204f894ef55d6353f9396d62
Tais <72388962+TeaThais@users.noreply.github.com>
2022-09-01 20:54:37 +0300

    Update ProfileInfo.jsx

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import classes from "./ProfileInfo.module.css";
 
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
     return <div>
         <div>
-            <img className={classes.picture} src="https://images.all-free-download.com/images/graphiclarge/beach_cloud_dawn_horizon_horizontal_landscape_ocean_601821.jpg" alt="pic"/>
+            <img className={props.className} src={props.background} alt=''/>
         </div>
         <div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS4aC4cPo8HWhUVKG2iKUxhOAl9aGKqflRBg&usqp=CAU" alt=""/>
+            <img src={props.avatarPic} alt=""/>
         </div>
     </div>
 };
