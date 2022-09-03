a438e39cf949678ab28f13a5e60c5a8ae8798c1c
Tais <72388962+TeaThais@users.noreply.github.com>
2022-09-04 00:09:21 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import classes from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import PostItem from "./MyPosts/Post/PostItem/PostItem";
+
 
 const Profile = (props) => {
 
     return <div>
-        <ProfileInfo className={classes.picture} background={props.backgroundPic} avatarPic={props.avatarPic}/>
+        <ProfileInfo className={classes.picture} backgroundPic={props.state.backgroundPic} avatarPic={props.state.avatarPic}/>
         <h3>Posts</h3>
-        <MyPosts postItems={props.postItems}/>
+        <MyPosts postItems={props.state.postItems}/>
     </div>
 }
 
