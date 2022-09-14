fbc96ed29edc263f2cafe5c3c5a08f5d5017486d
Tais <72388962+TeaThais@users.noreply.github.com>
2022-09-14 14:41:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
+import {addPost} from "./redux/state";
 
 
 ReactDOM.render(
     <BrowserRouter>
         <React.StrictMode>
-            <App state={state}/>
+            <App state={state} addPost={addPost}/>
         </React.StrictMode>
     </BrowserRouter>,document.getElementById('root')
 );
