cc25453408f209055f36f070096fe525bc95e4b2
Tais <72388962+TeaThais@users.noreply.github.com>
2022-09-24 03:22:43 +0300

    Update MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
 import postItem from "./Post/PostItem/PostItem";
+import {updateTextarea} from "../../../redux/state";
 
 const MyPosts = (props) => {
     return <div>
-            <Post postItems={props.postItems} addPost={props.addPost}/>
+            <Post postItems={props.postItems} addPost={props.addPost} updateTextarea={props.updateTextarea}/>
         </div>
 }
 
