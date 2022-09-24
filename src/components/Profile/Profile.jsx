da3dc7c9f8a3023b3391315b5a6b2033640949fa
Tais <72388962+TeaThais@users.noreply.github.com>
2022-09-24 03:22:50 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import classes from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import {updateTextarea} from "../../redux/state";
 
 
 const Profile = (props) => {
 
     return <div>
-        <ProfileInfo className={classes.picture} backgroundPic={props.state.backgroundPic} avatarPic={props.state.avatarPic}/>
+        <ProfileInfo className={classes.picture} backgroundPic={props.profilePage.backgroundPic} avatarPic={props.profilePage.avatarPic}/>
         <h3>Posts</h3>
-        <MyPosts postItems={props.state.postItems} addPost={props.addPost}/>
+        <MyPosts postItems={props.profilePage.postItems}
+                 addPost={props.addPost}
+                 updateTextarea={props.updateTextarea}/>
     </div>
 }
 
