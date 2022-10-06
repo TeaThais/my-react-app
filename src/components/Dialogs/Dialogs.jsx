89865904e6ed819a175d07c01e56627a7c0a602c
Tais <72388962+TeaThais@users.noreply.github.com>
2022-10-06 17:15:17 +0300

    Update Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import classes from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-import store, {reloadTextareaCreator, sendMessageCreator} from "../../redux/store";
+import {reloadTextareaCreator, sendMessageCreator} from "../../redux/dialogs-reducer";
+
 
 const Dialogs = (props) => {
 
