3fdd78f2db9fa5f6ab51e0abda8b84f6ad75580f
Tais <72388962+TeaThais@users.noreply.github.com>
2022-10-13 02:20:52 +0300

    Update DialogsContainer.jsx

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import {reloadTextareaCreator, sendMessageCreator} from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
+import Store from "../../redux/store";
+import StoreContext from "../../StoreContext";
 
 
 const DialogsContainer = (props) => {
 
-    let state = props.store.getState().dialogsPage;
-
-    let onSendMessageClick = ()=> {
-        props.store.dispatch(sendMessageCreator());
-    }
-
-    let onAreaChange = (messageBody)=> {
-         props.store.dispatch(reloadTextareaCreator(messageBody));
-    }
+    // let state = props.store.getState().dialogsPage;
 
     return (
-        <Dialogs dialogs={state.dialogs}
-                 messagesData={state.messagesData}
-                 messageText={state.messageText}
+        <StoreContext.Consumer>
+            {(store) => {
+                let state = store.getState();
+
+                let onSendMessageClick = ()=> {
+                    store.dispatch(sendMessageCreator());
+                }
+
+                let onAreaChange = (messageBody)=> {
+                    store.dispatch(reloadTextareaCreator(messageBody));
+                }
+
+            return <Dialogs dialogs={state.dialogsPage.dialogs}
+                 messagesData={state.dialogsPage.messagesData}
+                 messageText={state.dialogsPage.messageText}
                  addMessage={onSendMessageClick}
-                 reloadTextarea={onAreaChange}/>
+                 reloadTextarea={onAreaChange}/>}
+        }
+        </StoreContext.Consumer>
     )
 };
 
