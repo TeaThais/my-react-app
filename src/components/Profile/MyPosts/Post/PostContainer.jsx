c04fbd3e6a75b53f3c9a813caede9e3f5366b9f8
Tais <72388962+TeaThais@users.noreply.github.com>
2022-10-13 02:21:09 +0300

    Update PostContainer.jsx
    Context API

--- src/components/Profile/MyPosts/Post/PostContainer.jsx
+++ src/components/Profile/MyPosts/Post/PostContainer.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import {addPostActionCreator, updateTextareaActionCreator} from "../../../../redux/profile-reducer";
 import Post from "./Post";
+import StoreContext from "../../../../StoreContext";
 
 
 const PostContainer = (props) => {
 
-    let state = props.store.getState();
-
-    let onAddPost = ()=> {
-        props.store.dispatch(addPostActionCreator());
-    }
-
-    let onAreaChange = (text)=> {
-        props.store.dispatch(updateTextareaActionCreator(text));
-    }
-
+   // let state = props.store.getState();
     //console.log('props: ', props )
     return (
-        <Post postItems={state.profilePage.postItems}
-              newPostText={state.profilePage.newPostText}
-              updateTextarea={onAreaChange}
-              addPost={onAddPost}/>
+        <StoreContext.Consumer>
+            {(store) => {
+                let state = store.getState();
+
+                let onAddPost = ()=> {
+                    store.dispatch(addPostActionCreator());
+                }
+
+                let onAreaChange = (text)=> {
+                    store.dispatch(updateTextareaActionCreator(text));
+                }
+                return <Post postItems={state.profilePage.postItems}
+                             newPostText={state.profilePage.newPostText}
+                             updateTextarea={onAreaChange}
+                             addPost={onAddPost}/>
+            }
+
+        }
+        </StoreContext.Consumer>
     )
 }
 // <PostItem pic={<img className='img' src={Picture[0].link} alt=""/>} name={postItems[0].name} message={postItems[0].message} likes={postItems[0].likes}/>
