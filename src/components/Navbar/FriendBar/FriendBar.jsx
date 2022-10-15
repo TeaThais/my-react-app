bbfdce58c49026a5e0b5fc78f7fee33ee7f5d6e8
Tais <72388962+TeaThais@users.noreply.github.com>
2022-10-16 02:23:07 +0300

    Update FriendBar.jsx

--- src/components/Navbar/FriendBar/FriendBar.jsx
+++ src/components/Navbar/FriendBar/FriendBar.jsx
@@ -5,7 +5,7 @@ import classes from "./FriendBar.module.css"
 
 const FriendBar = (props) => {
     let friendAvatar = props.navbarPic
-        .map(av => <Friend pict={<img className='img' src={av.link} alt=""/>} />)
+        .map(av => <Friend  key={av.id} pict={<img className='img' src={av.link} alt=""/>} />)
 
     return <div className={classes.avatar}>
         {friendAvatar}
