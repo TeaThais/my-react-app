2281f1a463c027e4c32924cadd0a5d65c0b6c166
Tais <72388962+TeaThais@users.noreply.github.com>
2022-12-13 14:19:22 +0300

    Update users-reducer.js

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -2,6 +2,7 @@ const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 
 let initialState = {
     users: [
@@ -38,8 +39,8 @@ let initialState = {
         //     location: {country: ' Serbia', city: 'Belgrade'}
         // }
     ],
-    pageSize: 10,
-    totalUsersCount: 70,
+    pageSize: 100,
+    totalUsersCount: 0,
     currentPage: 1
 }
 
@@ -70,11 +71,15 @@ const usersReducer = (state=initialState, action) => {
             }
         }
         case SET_USERS: {
-            return {...state, users: [...action.users]}
+            return {...state, users: action.users}
         }
 
         case SET_CURRENT_PAGE: {
             return {...state, currentPage: action.currentPage}
+        }
+
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.totalCount}
         }
 
         default:
@@ -86,5 +91,6 @@ export const followAC = (userID) => ({type: FOLLOW, userID})
 export const unfollowAC = (userID) => ({type: UNFOLLOW, userID})
 export const setUsersAC = (users) => ({type: SET_USERS, users})
 export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setTotalUsersCountAC = (totalCount) => ({type: SET_TOTAL_USERS_COUNT, totalCount})
 
 export default usersReducer;
