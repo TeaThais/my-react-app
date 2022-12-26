6c3dab58e2284611d6fb8915bed5e917dffe37c9
Tais <72388962+TeaThais@users.noreply.github.com>
2022-12-26 23:26:30 +0300

    Update ProfileInfo.jsx

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,13 +3,20 @@ import classes from "./ProfileInfo.module.css";
 
 
 const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <div>
+            <div>
+                <img className={props.className} src={props.backgroundPic} alt=''/>
+            </div>
+            <div>
+                <img src={props.avatarPic} alt=""/>
+
+            </div>
+        </div>
+    }
+
     return <div>
-        <div>
-            <img className={props.className} src={props.backgroundPic} alt=''/>
-        </div>
-        <div>
-            <img src={props.avatarPic} alt=""/>
-        </div>
+        <img src={props.profile.photos.large} alt=""/>
     </div>
 };
 
