601177ab81cd6f7774613d43a8288ad9e5d001fa
Tais <72388962+TeaThais@users.noreply.github.com>
2023-01-09 02:58:07 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,8 +3,6 @@ import classes from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
-
-
 const Profile = (props) => {
 
     return <div>
