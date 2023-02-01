c4595b71fa28ddcacbe17080e604b6909157f289
Tais <72388962+TeaThais@users.noreply.github.com>
2023-02-02 02:24:15 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from "../../logo.svg";
 import classes from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return <header className={classes.header}>
         <img src={logo} className={classes.app_logo} alt="pict"/>
+
+        <div className={classes.loginBlock}>
+            <NavLink to={'/login'}>Login</NavLink>
+        </div>
     </header>
 }
 
