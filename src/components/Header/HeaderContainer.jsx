d9d6672ea1012f46b1f98575c561dbcf8bcb295f
Tais <72388962+TeaThais@users.noreply.github.com>
2023-02-08 00:46:17 +0300

    Update HeaderContainer.jsx

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "./Header";
 import axios from "axios";
+import {connect} from "react-redux";
+import {setAuthUserData} from "../../redux/auth-reducer";
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
+            withCredentials: true
+        })
             .then(response => {
-                this.props.setUserProfile(response.data);
+                if (response.data.resultCode === 0) {
+                    let {id, email, login} = response.data.data
+                    this.props.setAuthUserData(id, email, login);
+                }
             })
     }
 
@@ -15,4 +22,9 @@ class HeaderContainer extends React.Component {
     }
 }
 
-export default HeaderContainer;
+const mapStateToProps = (state)=> ({
+    isAuth: state.auth.isAuth,
+    login: state.auth.login
+});
+
+export default connect(mapStateToProps, {setAuthUserData}) (HeaderContainer);
