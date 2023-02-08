965d83e6f502c0ce4f934a1755c47d8430f89569
Tais <72388962+TeaThais@users.noreply.github.com>
2023-02-08 18:02:47 +0300

    Update HeaderContainer.jsx

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -24,7 +24,8 @@ class HeaderContainer extends React.Component {
 
 const mapStateToProps = (state)=> ({
     isAuth: state.auth.isAuth,
-    login: state.auth.login
+    login: state.auth.login,
+    email: state.auth.email,
 });
 
 export default connect(mapStateToProps, {setAuthUserData}) (HeaderContainer);
